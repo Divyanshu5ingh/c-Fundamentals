cbbf43c8113757faa07e15b66f803451d4a924b1
Divu <d1403kr3@gmail.com>
2023-02-09 14:45:50 +0530

    Hello World

---
+++ BasicsNet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsNet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered {0}", num);
+
+            Console.WriteLine("Enter a double number");
+            double db =  Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("You entered {0}", db);
+            Console.ReadKey();
+        }
+    }
+}
