5176d113183ad10788f00e306ed7c1742e94dd59
Divu <d1403kr3@gmail.com>
2023-02-09 15:57:58 +0530

    Overloading

--- BasicsNet/Program.cs
+++ BasicsNet/Program.cs
@@ -8,15 +8,22 @@ namespace BasicsNet
 {
     class Program
     {
+        public static int overload(int a, int b)
+        {
+            return a + b;
+        }
+        public static int overload(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        
+       
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You entered {0}", num);
-
-            Console.WriteLine("Enter a double number");
-            double db =  Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("You entered {0}", db);
+            Console.WriteLine(overload(5, 6));
+            Console.WriteLine(overload(1, 2, 3));
+           
+            
             Console.ReadKey();
         }
     }
